d718406e5f5c44d9114b6309e83180320fd9154d
OctavioLuis <oct.luisg@gmail.com>
2019-12-12 12:19:51 -0600

    Displaying Lists with RecyclerView, Adapter, and ViewHolder RecyclerView

--- app/src/main/java/com/unsis/criminalintent/edu/CrimeListFragment.java
+++ app/src/main/java/com/unsis/criminalintent/edu/CrimeListFragment.java
@@ -1,9 +1,14 @@
 package com.unsis.criminalintent.edu;
 
+import android.app.TaskInfo;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,7 +27,7 @@ public class CrimeListFragment extends Fragment {
     /*Now that CrimeListFragment ’s view is set up, hook up the view to the fragment. Modify
 CrimeListFragment to use this layout file and to find the RecyclerView in the layout fil*/
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
         mCrimeRecyclerView= view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -34,9 +39,31 @@ CrimeListFragment to use this layout file and to find the RecyclerView in the la
 /*The next job is to define the ViewHolder that will inflate and own your layout. Define it as an inner
 class in CrimeListFragment .*/
     private class CrimeHolder extends RecyclerView.ViewHolder {
+
+    private TextView mTitleTextView;
+    private TextView mDateTextView;
+
+    private Crime mCrime;
+
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crimen_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crimen_date);
+
         }
+
+    public void bind(Crime crime) {
+        mCrime = crime;
+
+
+        Toast.makeText(getContext(), mCrime.getTitle(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getContext(), mCrime.getDate().toString(), Toast.LENGTH_LONG).show();
+       mTitleTextView.setText(mCrime.getTitle());
+        mDateTextView.setText(mCrime.getDate().toString());
+
+
+    }
     }
 
 /*.
@@ -44,6 +71,7 @@ CrimeHolder is all skin and bones right now. Later in the chapter, CrimeHolder w
 give it more work to do.
 With the ViewHolder defined, create the adapter.*/
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+
         private List<Crime> mCrimes;
 
         public CrimeAdapter(List<Crime> crimes) {
@@ -54,13 +82,15 @@ With the ViewHolder defined, create the adapter.*/
 overrides by putting your cursor on top of extends , keying in Option-Return (Alt+Enter), selecting
 Implement methods , and clicking OK.*/
         @Override
-        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public CrimeHolder onCreateViewHolder( ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             return new CrimeHolder(layoutInflater, parent);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull CrimeHolder holder, int position) {
+        public void onBindViewHolder( CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.bind(crime);
 
         }
 
